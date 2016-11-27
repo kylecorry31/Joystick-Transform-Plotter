d38e36c12b431cd98bbb266b7dac02cc30f24da2
Kyle Corry <kylecorry31@gmail.com>
2016-11-26 20:05:21 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Joystick Transform Plotter
+==========================
+
+A joystick transform function plotter for Python.
+Maps controller input through an equation and plots it as either a 2d representation of a 3d graph or a 2d graph for a single axis.
+
+```Python
+## examples
+plot3d(mag)
+
+def thresh(x, y, t):
+    if abs(x) < t:
+        x = 0
+    if abs(y) < t:
+        y = 0
+    return mag(x, y)
+
+plot3d(lambda x, y: thresh(x, y, 0.25))
+
+def square(x):
+    return x**2
+
+plot2d(square)
+plot2d(lambda x: x)
+
+def thresh2(x, t):
+    if abs(x) < t:
+        return 0
+    return x
+
+plot2d(lambda x: thresh2(x, 0.25))
+```
---
+++ joystick_transform_plotter.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import math
+
+def mag(x, y):
+    return math.sqrt(x**2 + y**2)
+
+def evalMatrix(fn, numpoints=10):
+    mat = createMatrix(numpoints, numpoints)
+    for i in range(len(mat)):
+            for j in range(len(mat)):
+                    y = i / float(numpoints-1) * 2 - 1
+                    x = j / float(numpoints-1) * 2 - 1
+                    mat[i][j] = fn(x, y)
+    return mat
+
+def createMatrix(rows, cols):
+    mat = []
+    for _ in range(rows):
+            row = []
+            for _ in range(cols):
+                    row.append([])
+            mat.append(row)
+    return mat
+
+def toColors(mat):
+    return list(map(lambda x: map(lambda y: y / math.sqrt(2), x), mat))
+
+def y_grid(numpoints):
+    mat = createMatrix(numpoints, numpoints)
+    for i in range(numpoints):
+            for j in range(numpoints):
+                    y = i / float(numpoints-1) * 2 - 1
+                    mat[i][j] = y
+    return mat
+
+def x_grid(numpoints):
+    mat = createMatrix(numpoints, numpoints)
+    for i in range(numpoints):
+            for j in range(numpoints):
+                    x = j / float(numpoints-1) * 2 - 1
+                    mat[i][j] = x
+    return mat
+
+
+def plot3d(fn, numpoints=100):
+    plt.scatter(x_grid(numpoints), y_grid(numpoints), c=toColors(evalMatrix(fn, numpoints)), edgecolors='face', vmin=0, vmax=1)
+    plt.show()
+
+def plot2d(fn, numpoints=1000):
+    mat = createMatrix(1, numpoints)
+    x_mat = createMatrix(1, numpoints)
+    for i in range(numpoints):
+        x = i / float(numpoints - 1) * 2 - 1
+        x_mat[0][i] = x
+        mat[0][i] = fn(x)
+    plt.scatter(x_mat, mat, c=mat, edgecolors='face', vmin=-1, vmax=1)
+    plt.show()
+
+    
